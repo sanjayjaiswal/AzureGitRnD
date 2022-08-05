4e158904029a2b616b8d154a60579e0ebcd47c3d
Sanjay Kumar Jaiswal <sanjaykuarjaiswal@gmail.com>
2022-08-05 18:56:24 +0530

    Add project files.

---
+++ AzureGitRnD/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AzureGitRnD.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
